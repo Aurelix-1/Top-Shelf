cd27b81af7eca8be535d295b4a0960ecfb30f0d6
bilall571 <latifkxanovabdulvoxid@gmail.com>
2026-07-17 15:59:14 +0500

    layloo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Home from "./router/Home/Home.jsx";
 import { Route, Routes } from "react-router-dom";
-import SingleRoute from './router/SingleRoute/SingleRoute.jsx';
+import SingleRoute from "./router/SingleRoute/SingleRoute.jsx";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/single" element={<SingleRoute />} />
       </Routes>
     </div>
   );
